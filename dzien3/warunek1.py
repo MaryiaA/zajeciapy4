029175e8930541290dd84017d6a9fb55cc1cb86a
MaryiaA <33104649+MaryiaA@users.noreply.github.com>
2017-11-09 20:35:02 +0100

    zajecie 2

---
+++ dzien3/warunek1.py
@@ -0,0 +1,25 @@
+if True :
+    pass
+
+#if 5 == 10 / 2:
+ #   print('wnetrze ifa')
+
+if 5 != 20 / 4:
+        print('Drugi if')
+
+elif 5 == 5 and 20 %2 == 1:
+    print("Drugi if, elif")
+
+elif 45 % 3 == 12:
+    print('elif modulo')
+
+else:
+    print('instrukcja domyslna')
+
+print('pierwsza instrukcja')
+
+if 5 == 10:
+    print('5 ==10')
+
+else:
+    print('else can be used')
