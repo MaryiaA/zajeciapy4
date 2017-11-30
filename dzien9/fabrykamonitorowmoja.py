7bf1511315b2cb9916f3ac778fa7e43f3bf49164
MaryiaA <33104649+MaryiaA@users.noreply.github.com>
2017-11-30 18:55:07 +0100

    push

--- dzien9/fabrykamonitorowmoja.py
+++ dzien9/fabrykamonitorowmoja.py
@@ -3,4 +3,12 @@ from dzien9.monitorm import Monitor
 monitorm import Monitor
 
 
-monitor1 = Monitor
+monitor1 = Monitor(19)
+
+print(monitor1.przekatna)
+monitor1.przekatna = 'dwadziescia jeden'
+print(monitor1.przekatna)
+
+
+monitor2 = Monitor(24)
+print(monitor2.przekatna)
